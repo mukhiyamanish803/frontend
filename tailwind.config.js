cc6b8a8a07d637cb7aa4c0454410c32d7a3f75c3
mukhiyamanish803 <mukhiyamanish803@gmail.com>
2025-06-23 20:11:27 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    // Add paths to all of your components and pages here
+  ],
+  theme: {
+    extend: {
+      // ...other extensions...
+    },
+  },
+  plugins: [
+    // ...other plugins...
+  ],
+};
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false, // ✅ Disable source maps in production
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
