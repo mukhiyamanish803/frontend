2a6a903febfaf5e167cf0ce6ac7dc7abeb028c13
Manish Mukhiya <mukhiyamanish803@gmail.com>
2025-06-24 00:31:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "employara",
   build: {
     sourcemap: false, // ✅ Disable source maps in production
   },
